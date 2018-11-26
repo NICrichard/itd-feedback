28c8bcb631e8447f14ca15a490d2e1c8627a6f65
Richard Tuttle <richard.tuttle@accessidaho.org>
2018-11-26 15:55:29 -0700

    updates

--- App.vue
+++ App.vue
@@ -5,40 +5,43 @@
         <button class="btn btn-default" data-dismiss="modal">x</button>
       </div>
     </div>
-    <div class="row text-center">
-      <div class="col-12">
-        <h1 class="question">How would you rate your experience?</h1>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-          <i
-            :class="{'flaticon-smiley':true, 'selected':(isActive === 1)}"
-            @click="clicked('happy')"
-          ></i>
+
+    <div class="card">
+      <div class="card-header">Help us improve ITD online services</div>
+      <div class="card-body">
+        <h5
+          class="card-title"
+        >How would you prefer to do business with ITD (example, renew your license)?</h5>
+        <div class="form-check">
+          <input type="radio" name="itd-feedback" id="itd1" value="app" class="form-check input">
+          <label for="itd1" class="form-check-label">Via an app downloaded to my device</label>
         </div>
-        <div class="col-md-4">
-          <i :class="{'flaticon-anger':true, 'selected':(isActive === 2)}" @click="clicked('meh')"></i>
+        <div class="form-check">
+          <input
+            type="radio"
+            name="itd-feedback"
+            id="itd2"
+            value="website"
+            class="form-check input"
+          >
+          <label for="itd2" class="form-check-label">Online through a secure website</label>
         </div>
-        <div class="col-md-4">
-          <i
-            :class="{'flaticon-sad-smiley':true, 'selected':(isActive === 3)}"
-            @click="clicked('sad')"
-          ></i>
+        <div class="form-check">
+          <input type="radio" name="itd-feedback" id="itd3" value="person" class="form-check input">
+          <label for="itd3" class="form-check-label">In person</label>
         </div>
-      </div>
-      <div class="row" v-show="showMe">
-        <div class="col-xs-12 pb-5">
-          <label class="sr-only" for="feedback comments">Feedback comments</label>
+        <div class="form-group">
+          <label for="comments">Comment box, optional</label>
           <textarea
+            name="itd-comment"
+            id="comments"
+            cols="30"
+            rows="10"
             class="form-control"
-            rows="3"
-            id="feedbackText"
-            placeholder="enter your additional feedback here"
-            @blur="msg"
-            aria-label="feedback comments"
+            maxlength="300"
           ></textarea>
         </div>
-        <div class="col-xs-12 text-right" style="padding-top:10px">
+        <div class="card-footer">
           <button class="btn btn-primary" data-dismiss="modal">Submit</button>
         </div>
       </div>
@@ -53,94 +56,9 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import axios from "axios";
 var $ = jQuery;
 export default {
-  name: "app",
+  name: "itd",
   data: function() {
-    return {
-      showMe: false,
-      mood: "",
-      isActive: 0,
-      token: "",
-      changes: ""
-    };
-  },
-  methods: {
-    clicked: function(mood) {
-      let self = this;
-      if (mood === "happy") {
-        this.isActive = 1;
-      } else if (mood === "meh") {
-        this.isActive = 2;
-      } else if (mood === "sad") {
-        this.isActive = 3;
-      } else {
-        this.isActive = 0;
-      }
-      this.mood = mood;
-      this.showMe = true;
-      if (this.changes >= 0) {
-        this.changes = this.changes + 1;
-      }
-      self.callOut();
-    },
-    msg: function() {
-      var self = this;
-      if (this.changes >= 0) {
-        this.changes = this.changes + 1;
-      }
-      self.callOut();
-    },
-    callOut: function() {
-      var headers = {
-        "Content-Type": "application/json;charset=UTF-8",
-        "Access-Control-Allow-Origin": "*",
-        "X-Content-Type-Options": "nosniff"
-      };
-      if (this.token !== "") {
-        axios
-          .post(
-            "https://y1dtrwmk40.execute-api.us-east-1.amazonaws.com/UAT/RateMyTater-test",
-            {
-              tater_id: this.token,
-              rating: this.mood.toString(),
-              text: $("textarea#feedbackText").val(),
-              changes: this.changes,
-              appName: window.appName
-            },
-            headers
-          )
-          .then(response => {
-            this.token = response.data.tater_id;
-            localStorage.setItem("accessidaho", this.token);
-          })
-          .catch(e => {
-            console.error(e);
-          });
-      } else {
-        axios
-          .post(
-            "https://y1dtrwmk40.execute-api.us-east-1.amazonaws.com/UAT/RateMyTater-test",
-            {
-              rating: this.mood.toString(),
-              text: $("textarea#feedbackText").val(),
-              appName: window.appName
-            },
-            headers
-          )
-          .then(response => {
-            this.token = response.data.tater_id;
-            this.changes = response.data.changes;
-            localStorage.setItem("accessidaho", this.token);
-          })
-          .catch(e => {
-            console.error(e);
-          });
-      }
-    }
-  },
-  created: function() {
-    if (localStorage.getItem("accessidaho")) {
-      this.token = localStorage.getItem("accessidaho");
-    }
+    return {};
   }
 };
 </script>
@@ -154,8 +72,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-
-@import "icons/flaticon.css";
 
 .container {
   max-width: 500px;
@@ -173,13 +89,5 @@ export default {
 
 textarea {
   resize: none;
-}
-
-.selected {
-  color: #012b72;
-}
-
-.question {
-  font-size: 2.5rem;
 }
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <div id="app" class="container">
+  <div id="itd" class="container">
     <div class="row">
       <div class="col mt-0 text-right">
         <button class="btn btn-default" data-dismiss="modal">x</button>
       </div>
     </div>
-    <div class="row text-center">
-      <div class="col-12"><h1 class="question">How would you rate your experience?</h1></div>
-          <div class="row">
-              <div class="col-md-4"><i :class="{'flaticon-smiley':true, 'selected':(isActive === 1)}" @click="clicked('happy')"></i></div>
-              <div class="col-md-4"><i :class="{'flaticon-anger':true, 'selected':(isActive === 2)}" @click="clicked('meh')"></i></div>
-              <div class="col-md-4"><i :class="{'flaticon-sad-smiley':true, 'selected':(isActive === 3)}" @click="clicked('sad')"></i></div>
-          </div>
-          <div class="row" v-show="showMe">
-            <div class="col-xs-12 pb-5">
-                <label class="sr-only" for="feedback comments">Feedback comments</label><textarea class="form-control" rows="3" id="feedbackText" placeholder="enter your additional feedback here" @blur="msg" aria-label="feedback comments"></textarea>
+    <div class="row">
+      <div class="card">
+        <div class="card-header">Help us improve ITD online services</div>
+        <div class="card-body">
+          <div
+            class="card-title"
+          >How would you prefer to do business with ITD (example, renew your license)?</div>
+          <div class="card-text">
+            <div class="form-check">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="itd-feedback"
+                value="app"
+                id="itd1"
+              >
+              <label for="itd1" class="form-check-label">Via an app downloaded to my device</label>
             </div>
-            <div class="col-xs-12 text-right" style="padding-top:10px">
-              <button class="btn btn-primary" data-dismiss="modal">Submit</button>
+            <div class="form-check">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="itd-feedback"
+                value="website"
+                id="itd2"
+              >
+              <label for="itd2" class="form-check-label">Online through a secure website</label>
+            </div>
+            <div class="form-check">
+              <input
+                type="radio"
+                class="form-check-input"
+                name="itd-feedback"
+                value="in-person"
+                id="itd3"
+              >
+              <label for="itd3" class="form-check-label">In person</label>
             </div>
           </div>
+          <div class="form-group">
+            <label for="comments">Comments, optional</label>
+            <textarea
+              name="comments"
+              id="comments"
+              maxlength="300"
+              cols="30"
+              rows="10"
+              class="form-control"
+            ></textarea>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary">submit</button>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
@@ -31,95 +71,7 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import axios from "axios";
 var $ = jQuery;
 export default {
-  name: "app",
-  data: function() {
-    return {
-      showMe: false,
-      mood: "",
-      isActive: 0,
-      token: "",
-      changes: ""
-    };
-  },
-  methods: {
-    clicked: function(mood) {
-      let self = this;
-      if (mood === "happy") {
-        this.isActive = 1;
-      } else if (mood === "meh") {
-        this.isActive = 2;
-      } else if (mood === "sad") {
-        this.isActive = 3;
-      } else {
-        this.isActive = 0;
-      }
-      this.mood = mood;
-      this.showMe = true;
-      if (this.changes >= 0) {
-        this.changes = this.changes + 1;
-      }
-      self.callOut();
-    },
-    msg: function() {
-      var self = this;
-      if (this.changes >= 0) {
-        this.changes = this.changes + 1;
-      }
-      self.callOut();
-    },
-    callOut: function() {
-      var headers = {
-        "Content-Type": "application/json;charset=UTF-8",
-        "Access-Control-Allow-Origin": "*",
-        "X-Content-Type-Options": "nosniff"
-      };
-      if (this.token !== "") {
-        axios
-          .post(
-            "https://y1dtrwmk40.execute-api.us-east-1.amazonaws.com/UAT/RateMyTater-test",
-            {
-              tater_id: this.token,
-              rating: this.mood.toString(),
-              text: $("textarea#feedbackText").val(),
-              changes: this.changes,
-              appName: window.appName
-            },
-            headers
-          )
-          .then(response => {
-            this.token = response.data.tater_id;
-            localStorage.setItem("accessidaho", this.token);
-          })
-          .catch(e => {
-            console.error(e);
-          });
-      } else {
-        axios
-          .post(
-            "https://y1dtrwmk40.execute-api.us-east-1.amazonaws.com/UAT/RateMyTater-test",
-            {
-              rating: this.mood.toString(),
-              text: $("textarea#feedbackText").val(),
-              appName: window.appName
-            },
-            headers
-          )
-          .then(response => {
-            this.token = response.data.tater_id;
-            this.changes = response.data.changes;
-            localStorage.setItem("accessidaho", this.token);
-          })
-          .catch(e => {
-            console.error(e);
-          });
-      }
-    }
-  },
-  created: function() {
-    if (localStorage.getItem("accessidaho")) {
-      this.token = localStorage.getItem("accessidaho");
-    }
-  }
+  name: "itd"
 };
 </script>
 
@@ -133,8 +85,6 @@ export default {
   margin-top: 60px;
 }
 
-@import "icons/flaticon.css";
-
 .container {
   max-width: 500px;
   height: auto;
@@ -145,19 +95,7 @@ export default {
   text-align: center;
 }
 
-.feedback {
-  padding-top: 20px;
-}
-
 textarea {
   resize: none;
-}
-
-.selected {
-  color: #012b72;
-}
-
-.question {
-  font-size: 2.5rem;
 }
 </style>
